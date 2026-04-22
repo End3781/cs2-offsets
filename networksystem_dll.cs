43897461ea2550a3ffa05063fc4806509b7a3b73
End3781 <End3781@github.com>
2026-04-22 23:24:41 +0500

    22.04.2026 Update

--- networksystem_dll.cs
+++ networksystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-21 11:16:07.387965900 UTC
+// 2026-04-22 18:23:20.335012200 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: networksystem.dll
